8a78fe7472689adf868f8d858368c4fd925854d5
shashank748047 <245122748047@mvsrec.edu.in>
2025-07-23 12:52:51 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,12 @@
+Vi ArithmeticOperations.java
+public class ArithmeticOperations {
+public static void main(String[] args) {
+int a = 20;
+int b = 10;
+System.out.println("Addition = " + (a + b));
+System.out.println("Subtraction = " + (a - b));
+System.out.println("Multiplication = " + (a * b));
+System.out.println("Division = " + (a / b));
+}
+}
+
